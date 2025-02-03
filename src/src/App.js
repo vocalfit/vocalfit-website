c8e309fc2c84fa5823d7dd4b880b06fdbb56ee05
vocalfit <gusgh3133@naver.com>
2025-02-04 00:53:03 +0900

    Update App.js

--- src/src/App.js
+++ src/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import "./index.css";
 
 function App() {
-  return <h1>Welcome to Vocalfit Studio!</h1>;
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Vocalfit Studio!</h1>
+    </>
+  );
 }
 
 export default App;
