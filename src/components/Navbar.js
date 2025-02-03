32b48c833c90779c9ce11c7e82368ade25939864
vocalfit <gusgh3133@naver.com>
2025-02-04 00:40:06 +0900

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", background: "#333", color: "white" }}>
+      <h2>Vocalfit Studio</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
